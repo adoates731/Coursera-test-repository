1ebc4c79dcc5e121fed788fee1f3ffa35ecc753e
adoates731 <119081615+adoates731@users.noreply.github.com>
2022-12-24 14:01:22 -0500

    Update script.js

--- Module 5 Assignment/js/script.js
+++ Module 5 Assignment/js/script.js
@@ -116,9 +116,9 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+      // var homeHtmlToInsertIntoMainPage = ....    menuItemsUrl + categoryShort + ".json"
 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", 'chosenCategoryShortName');
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", homeHtmlUrl + chosenCategoryShortName + ".json");
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
